9da67953fe5a5393370397b93edf6a73ab5c8ac6
trollgugui13 <84552703+trollgugui13@users.noreply.github.com>
2022-11-23 08:55:26 -0300

    Add files via upload

--- resources/views/figurinhas/create.blade.php
+++ resources/views/figurinhas/create.blade.php
@@ -6,7 +6,7 @@
 
     <div class="mb-2">
         <label for="nome" class="form-label">Nome</label>
-        <input type="text" name="nome" class="form-control" id="nome">
+        <input type="text" name="nome" class="form-control" id="nome" value="Info">
     </div>
     <div class="mb-2">
         <label for="foto" class="form-label">Foto</label>
@@ -14,11 +14,11 @@
     </div>
     <div class="mb-2">
         <label for="dtnasc" class="form-label">Data de Nascimento</label>
-        <input type="date" name="dtnasc" class="form-control" id="dtnasc">
+        <input type="date" name="dtnasc" class="form-control" id="dtnasc" value="1900-01-01">
     </div>
     <div class="mb-2">
         <label for="nt" class="form-label">Naturalidade</label>
-        <input type="text" name="nt" class="form-control" id="nt">
+        <input type="text" name="nt" class="form-control" id="nt" value="Bento">
     </div>
     <a class="btn btn-danger" href="/figurinhas">Voltar</a>
     <button type="submit" class="btn btn-primary">Enviar</button>
--- resources/views/figurinhas/index.blade.php
+++ resources/views/figurinhas/index.blade.php
@@ -7,9 +7,10 @@
             <a href="https://ifrs.edu.br "><img class="logo " src="{{asset('/img/ifrs.png')}}"></a>
             <li></li>
 
-            <li><button id="novo" type="button" class="btn-new btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal1">
-                    Comprar Pacote
-                </button>
+            <li>
+                <form action='figurinhas/fcreate'>
+                    <input id="novo" type="submit" value='Criar Figurinha' class="btn-new btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal1">
+                </form>
             </li>
 
             <li><button id="novo" type="button" class="btn-new btn-success " data-bs-toggle="modal" data-bs-target="#exampleModal2">
@@ -36,44 +37,27 @@
     </nav>
 </div>
 
-@foreach($figurinhas as $figurinha)
-<div class="container ">
-    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-5 mt-5">
-        <div class="card h-100">
-            <img src="{{asset('/imagens')}}/{{$figurinha->foto}}" width="350" height="272" class="card-img-top mt-5">
-            <div class="card-body bg-transparent mb-2">
-                <h5 class="card-text text-center">Nome: {{$figurinha->nome}}</h5>
-                <h5 class="card-text text-center">Data de Nascimento: {{$figurinha->dtnasc}}</h5>
-                <h5 class="card-text text-center">Naturalidade: {{$figurinha->naturalidade}}</h5>
-            </div>
-        </div>
-    </div>
-</div>
-@endforeach
 
-<section class="bg-light py-4 my-5">
+
+<section class="bg-light py-4 my-5 rounded">
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h2 class="mb-3 text-primary">Figurinhas Do Infoderão</h2>
+                <h2 class="mb-3 text-black text-center">Figurinhas Do Infoderão</h2>
             </div>
-
             @foreach($figurinhas as $figurinha)
-            <div class="col-md-4 col-lg-3">
-                <div class="card my-2">
-
-                    <img src="{{asset('/imagens')}}/{{$figurinha->foto}}" class="card-img-top"  width="300 " height="272" ">
-
+            <div class="col-md-3 col-lg-3">
+                <div class="card my-3">
+                    <img src="{{asset('/imagens')}}/{{$figurinha->foto}}" class="rounded mx-auto mt-3 card-image-top" alt="thumbnail" width="270" height="252">
                     <div class=" card-body">
-                    <h3 class="card-title"><a href="#" class="text-secondary">Nome: {{$figurinha->nome}}</a></h3>
+                    <input type='hidden' value='{{$figurinha->ID}}' >
+                    <h3 class="card-title">Nome: {{$figurinha->nome}}</h3>
                     <p class="card-text">Data de Nascimento: {{$figurinha->dtnasc}} </p>
                     <p class="card-text">Naturalidade: {{$figurinha->naturalidade}} </p>
                 </div>
             </div>
         </div>
-
         @endforeach
-    </div>
     </div>
 </section>
 
