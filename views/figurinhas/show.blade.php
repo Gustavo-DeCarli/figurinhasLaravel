e3935a916669e5ff89debd908c53b97e1850b371
PedritoFerrrari <105124188+PedritoFerrrari@users.noreply.github.com>
2022-12-14 08:55:26 -0300

    Delete views directory

--- views/figurinhas/show.blade.php
+++
@@ -1,6 +0,0 @@
-<ul>
-    <li>ID: {{$figurinhas->id}}</li>
-    <li>Nome: {{$figurinhas->nome}}</li>
-    <li>Nome Reduzido: {{$figurinhas->nome_reduzido}}</li>
-</ul>
-<a href="/figurinhas">Voltar</a>
